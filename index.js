ed91a522586cb2cc542dd6d9cfcb7bf9d65f74b7
Rayokish <159958022+Rayokish@users.noreply.github.com>
2025-04-13 20:44:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const host = '0.0.0.0';
 const apiKey = "AIzaSyDL8lTQK78cwDfySVT_8JDbDXkgJyUcfV4"; // Replace with your actual API key
 const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-1.5-pro-latest",
   generationConfig: {
     temperature: 0.3,
     topP: 0.95,
